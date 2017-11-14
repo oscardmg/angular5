105cc1a03e271b237c2318246ecfdae9c917e5dc
Alberto Basalo <albertobasalo@hotmail.com>
2017-11-14 19:34:46 +0100

    operations form table model

--- 3-data/cash-flow/src/app/views/operations/list.component.ts
+++ 3-data/cash-flow/src/app/views/operations/list.component.ts
@@ -4,13 +4,8 @@ import { Component, OnInit } from "@angular/core";
   selector: "cf-list",
   template: `
     <p>
-      list works!
+      list is a work in progress!
     </p>
-    <ul>
-      <li><a routerLink="/operations/271">Number e</a></li>
-      <li><a routerLink="/operations/314">Pi</a></li>
-      <li><a routerLink="/operations/667">Gravitational Constant</a></li>
-    </ul>
   `,
   styles: []
 })
