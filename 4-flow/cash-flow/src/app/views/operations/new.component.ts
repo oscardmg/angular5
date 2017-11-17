fd65b728f20453cf1fde3eed86164bae279c435a
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-11-17 13:15:40 +0100

    split new view to list component

--- 4-flow/cash-flow/src/app/views/operations/new.component.ts
+++ 4-flow/cash-flow/src/app/views/operations/new.component.ts
@@ -28,27 +28,6 @@ import { Operation } from "./operation";
     Number of Operations:{{ numberOfOperations }}
     <em>Current: {{ operation | json }}</em>
   </blockquote>
-  <table *ngIf="numberOfOperations>0;else emptyList">
-    <thead>
-      <tr>
-        <th>Description</th>
-        <th>Kind</th>
-        <th>Amount</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr *ngFor="let operation of operations">
-        <td>{{ operation.description }}</td>
-        <td>{{ operation.kind }}</td>
-        <td>{{ operation.amount | number:'7.2-2' }}</td>
-        <td><button (click)="deleteOperation(operation)">Delete</button> </td>
-      </tr>
-    </tbody>
-  </table>
-  <ng-template #emptyList>
-    <h3>No operations yet.</h3>
-  </ng-template>
   `,
   styles: []
 })
